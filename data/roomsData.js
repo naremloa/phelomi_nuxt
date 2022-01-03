16e6bb91a26555165edccefe26df0845c58d4a2a
charlie <charlie@lvguanqingdeMacBook-Pro.local>
2022-01-03 23:22:55 +0800

    補上其餘房間圖片，添加所有房間外頁

--- data/roomsData.js
+++ data/roomsData.js
@@ -9,6 +9,8 @@ export default [
     name: '星空號愛琴海四人房',
     description: '愛琴海',
     imgOptions: [
+      // 這個 A001外頁 長得和 A003 一模一樣
+      '~/assets/rooms/afas/A001-cover.jpg',
       '~/assets/rooms/afas/A002.jpg',
       '~/assets/rooms/afas/A003.jpg',
       '~/assets/rooms/afas/A004.jpg'
@@ -24,6 +26,7 @@ export default [
     name: '星空號愛爾蘭海雙人房',
     description: '愛爾蘭海',
     imgOptions: [
+      '~/assets/rooms/atis/C001-cover.jpg',
       '~/assets/rooms/atis/C002.jpg',
       '~/assets/rooms/atis/C003.jpg',
       '~/assets/rooms/atis/C004.jpg',
@@ -40,6 +43,7 @@ export default [
     name: '星空號珊瑚海雙人房',
     description: '珊瑚海',
     imgOptions: [
+      '~/assets/rooms/atcs/D001-cover.jpg',
       '~/assets/rooms/atcs/D002.jpg',
       '~/assets/rooms/atcs/D003.jpg',
       '~/assets/rooms/atcs/D004.jpg',
@@ -56,6 +60,7 @@ export default [
     name: '星空號太平洋雙人房',
     description: '太平洋',
     imgOptions: [
+      '~/assets/rooms/atpo/E001-cover.jpg',
       '~/assets/rooms/atpo/E002.jpg',
       '~/assets/rooms/atpo/E003.jpg',
       '~/assets/rooms/atpo/E004.jpg',
@@ -73,6 +78,7 @@ export default [
     name: '彩虹堡蘋果綠四人房',
     description: '蘋果綠',
     imgOptions: [
+      '~/assets/rooms/bfag/O001-cover.jpg',
       '~/assets/rooms/bfag/O002.jpg',
       '~/assets/rooms/bfag/O003.jpg',
       '~/assets/rooms/bfag/O004.jpg',
@@ -90,6 +96,7 @@ export default [
     name: '彩虹堡桔子橙四人房',
     description: '桔子橙',
     imgOptions: [
+      '~/assets/rooms/bfoo/K001-cover.jpg',
       '~/assets/rooms/bfoo/K002.jpg',
       '~/assets/rooms/bfoo/K003.jpg',
       '~/assets/rooms/bfoo/K004.jpg',
@@ -107,27 +114,11 @@ export default [
     name: '彩虹堡紫蘿蘭四人房',
     description: '紫蘿蘭',
     imgOptions: [
+      '~/assets/rooms/bfv/L001-cover.jpg',
       '~/assets/rooms/bfv/L002.jpg',
       '~/assets/rooms/bfv/L003.jpg',
       '~/assets/rooms/bfv/L004.jpg',
       '~/assets/rooms/bfv/L005.jpg'
-    ]
-  },
-  {
-    id: 'bfag',
-    type: 'r4',
-    short: {
-      content: '帶著童心未泯的雀躍與期待，探索彩虹堡裡的秘密基地',
-      img: '~/assets/rooms_block/b4-apple-green.jpg'
-    },
-    name: '彩虹堡蘋果綠四人房',
-    description: '蘋果綠',
-    imgOptions: [
-      '~/assets/rooms/bfag/O002.jpg',
-      '~/assets/rooms/bfag/O003.jpg',
-      '~/assets/rooms/bfag/O004.jpg',
-      '~/assets/rooms/bfag/O005.jpg',
-      '~/assets/rooms/bfag/O006.jpg'
     ]
   },
   {
@@ -140,6 +131,7 @@ export default [
     name: '彩虹堡可可棕四人房',
     description: '可可棕',
     imgOptions: [
+      '~/assets/rooms/bfc/P001-cover.jpg',
       '~/assets/rooms/bfc/P002.jpg',
       '~/assets/rooms/bfc/P003.jpg',
       '~/assets/rooms/bfc/P004.jpg',
@@ -156,11 +148,133 @@ export default [
     name: '彩虹堡珊瑚粉四人房',
     description: '珊瑚粉',
     imgOptions: [
+      '~/assets/rooms/bfcp/Q001-cover.jpg',
       '~/assets/rooms/bfcp/Q002.jpg',
       '~/assets/rooms/bfcp/Q003.jpg',
       '~/assets/rooms/bfcp/Q004.jpg',
       '~/assets/rooms/bfcp/Q005.jpg',
       '~/assets/rooms/bfcp/Q006.jpg'
     ]
-  }
+  },
+  {
+    id: 'beikebai',
+    type: '',
+    short: {
+      content: '',
+      img: '~/assets/rooms_block/beikebai.jpg'
+    },
+    name: '',
+    description: '貝殼白',
+    imgOptions: [
+      '~/assets/rooms/beikebai/I001-cover.jpg',
+      '~/assets/rooms/beikebai/I002.jpg',
+      '~/assets/rooms/beikebai/I003.jpg',
+      '~/assets/rooms/beikebai/I004.jpg',
+      '~/assets/rooms/beikebai/I005.jpg',
+      '~/assets/rooms/beikebai/I006.jpg',
+    ]
+  },
+  {
+    id: 'yinduyang',
+    type: '',
+    short: {
+      content: '',
+      img: '~/assets/rooms_block/yinduyang.jpg'
+    },
+    name: '',
+    description: '印度洋',
+    imgOptions: [
+      '~/assets/rooms/yinduyang/G001-cover.jpg',
+      '~/assets/rooms/yinduyang/G002.jpg',
+      '~/assets/rooms/yinduyang/G003.jpg',
+      '~/assets/rooms/yinduyang/G004.jpg',
+      '~/assets/rooms/yinduyang/G005.jpg',
+    ]
+  },
+  {
+    id: 'daxiyang',
+    type: '',
+    short: {
+      content: '',
+      img: '~/assets/rooms_block/daxiyang.jpg'
+    },
+    name: '',
+    description: '大西洋',
+    imgOptions: [
+      '~/assets/rooms/daxiyang/F001-cover.jpg',
+      '~/assets/rooms/daxiyang/F002.jpg',
+      '~/assets/rooms/daxiyang/F003.jpg',
+      '~/assets/rooms/daxiyang/F004.jpg',
+      '~/assets/rooms/daxiyang/F005.jpg',
+      '~/assets/rooms/daxiyang/F006.jpg',
+    ]
+  },
+  {
+    id: 'jialebihai',
+    type: '',
+    short: {
+      content: '',
+      img: '~/assets/rooms_block/jialebihai.jpg'
+    },
+    name: '',
+    description: '加勒比海',
+    imgOptions: [
+      '~/assets/rooms/jialebihai/B001-cover.jpg',
+      '~/assets/rooms/jialebihai/B002.jpg',
+      '~/assets/rooms/jialebihai/B003.jpg',
+      '~/assets/rooms/jialebihai/B004.jpg',
+    ]
+  },
+  {
+    id: 'limeihong',
+    type: '',
+    short: {
+      content: '',
+      img: '~/assets/rooms_block/limeihong.jpg'
+    },
+    name: '',
+    description: '栗梅紅',
+    imgOptions: [
+      '~/assets/rooms/limeihong/J001-cover.jpg',
+      '~/assets/rooms/limeihong/J002.jpg',
+      '~/assets/rooms/limeihong/J003.jpg',
+      '~/assets/rooms/limeihong/J004.jpg',
+      '~/assets/rooms/limeihong/J005.jpg',
+      '~/assets/rooms/limeihong/J006.jpg',
+    ]
+  },
+  {
+    id: 'yujinhuang',
+    type: '',
+    short: {
+      content: '',
+      img: '~/assets/rooms_block/yujinhuang.jpg'
+    },
+    name: '',
+    description: '郁金黃',
+    imgOptions: [
+      '~/assets/rooms/yujinhuang/M001-cover.jpg',
+      '~/assets/rooms/yujinhuang/M002.jpg',
+      '~/assets/rooms/yujinhuang/M003.jpg',
+      '~/assets/rooms/yujinhuang/M004.jpg',
+      '~/assets/rooms/yujinhuang/M005.jpg',
+    ]
+  },
+  {
+    id: 'tiankonglan',
+    type: '',
+    short: {
+      content: '',
+      img: '~/assets/rooms_block/tiankonglan.jpg'
+    },
+    name: '',
+    description: '天空藍',
+    imgOptions: [
+      '~/assets/rooms/tiankonglan/H001-cover.jpg',
+      '~/assets/rooms/tiankonglan/H002.jpg',
+      '~/assets/rooms/tiankonglan/H003.jpg',
+      '~/assets/rooms/tiankonglan/H004.jpg',
+      '~/assets/rooms/tiankonglan/H005.jpg',
+    ]
+  },
 ]
