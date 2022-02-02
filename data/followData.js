22730f5977dae2b5fadf09707470c8db1279cecb
naremloa <naremloa@gmail.com>
2022-02-02 17:32:24 +0800

    [feat] -  調整就要醬玩行程規劃和價格

--- data/followData.js
+++ data/followData.js
@@ -15,17 +15,17 @@ export default [
     article: [
       {
         img: follow1,
-        content: 'Day1. 本島北環島+夜釣小管<br >Day2. 七美藍洞南方四島賞鷗遊(含餐)<br >Day3. 悠游浮淺 +市區逛街去',
+        content: 'Day1. 本島北環島 + 夜釣小管<br >Day2. 七美藍洞南方四島賞鷗遊(含餐)<br >Day3. 悠游浮淺 + 市區逛街去',
         title: '跳島趣 A 行程   $ 2700 /人'
       },
       {
         img: follow2,
-        content: 'Day1. 本島北環島+ 悠游浮淺<br>Day2. 南海二島七美+望安<br>Day3. 海洋牧場+ 市區逛街去',
+        content: 'Day1. 本島北環島 + 悠游浮淺<br>Day2. 南海二島七美 + 望安<br>Day3. 海洋牧場 + 市區逛街去',
         title: '跳島趣 B行程   $ 2100  /人'
       },
       {
         img: follow3,
-        content: 'Day1. 本島北環島+ 夜釣小管<br>Day2. 悠游浮淺+金色雙島虎井、桶盤島<br>Day3. 海洋牧場+ 市區逛街去',
+        content: 'Day1. 本島北環島 + 夜釣小管<br>Day2. 悠游浮淺 + 金色雙島虎井、桶盤島<br>Day3. 海洋牧場 + 市區逛街去',
         title: '跳島趣 C行程   $ 2250  /人'
       }
     ]
@@ -36,18 +36,18 @@ export default [
     article: [
       {
         img: follow4,
-        content: 'Day1. 陽光沙灘水上活動＋ＢＢＱ吃到飽<br>Day2. 登海賊船浮潛+跳水<br>Day3 東海賞燕鷗+市區逛街去',
-        title: '愛玩水A行程   $ 2400   起/人'
+        content: 'Day1. 陽光沙灘水上活動 ＋ ＢＢＱ吃到飽<br>Day2. 獨木舟無人島跨島<br>Day3 東海賞燕鷗 + 市區逛街去',
+        title: '愛玩水A行程   $ 2500   起/人'
       },
       {
         img: follow5,
-        content: 'Day1. 海底漫步+浮淺<br>Day2.東海超值一日遊<br>Day3. 立式划漿SUP',
-        title: '愛玩水B行程   $ 3000   起/人'
+        content: 'Day1. 海底漫步 + SUP立式划槳<br>Day2. 東海超值一日遊<br>Day3. 悠遊浮淺',
+        title: '愛玩水B行程   $ 3400   起/人'
       },
       {
         img: follow6,
-        content: 'Day1. 陽光沙灘水上活動<br>Day2. 南方四島東吉島薰衣草森林浮潛+七美<br>Day3. 海洋牧場+ 市區逛街去',
-        title: '愛玩水C行程   $ 4100  起/人'
+        content: 'Day1. 風帆、衝浪板滑水、獨木舟體驗<br>Day2. 南方四島&東嶼萍薰衣草森林浮潛<br>Day3. 陽光沙灘水上活動 + 市區逛街去',
+        title: '愛玩水C行程   $ 4800  起/人'
       }
     ]
   },
@@ -57,18 +57,18 @@ export default [
     article: [
       {
         img: follow7,
-        content: 'Day1. 海底漫步+浮淺<br>Day2. 獨木舟跨島四合一+沙灘浪漫晚餐<br>Day3. 海洋牧場+ 市區逛街去',
-        title: '愛浪漫 A 行程   $ 2680   起/人'
+        content: 'Day1. 金色夕陽賞鷗遊登島姑婆嶼(含晚餐)<br>Day2. 獨木舟跨島 + 浪漫晚餐<br>Day3. 海洋牧場 + 市區逛街去',
+        title: '愛浪漫 A 行程   $ 2480   起/人'
       },
       {
         img: follow8,
-        content: 'Day1. 本島北環島+ 海上浪漫晚餐<br>Day2. 東海超值一日遊 + 沙灘浪漫晚餐<br>Day3. 南海二島七美+望安',
-        title: '愛浪漫 B 行程   $ 3230   起/人'
+        content: 'Day1. 透明獨木舟 + 夜釣小管&賞花火<br>Day2. 東海超值一日遊<br>Day3. 南海二島七美 + 望安',
+        title: '愛浪漫 B 行程   $ 3300   起/人'
       },
       {
         img: follow9,
-        content: 'Day1. 悠游浮淺+ 海上浪漫晚餐<br>Day2. 東海賞燕鷗+沙灘浪漫晚餐<br>Day3. 獨木舟跨島四合一',
-        title: '愛浪漫 C 行程   $ 3330   起/人'
+        content: 'Day1. 悠游浮淺 + 沙灘浪漫晚餐<br>Day2. 星光浪漫夜遊&夜釣&專人BBQ藝人駐唱<br>Day3. 同心協力SUP',
+        title: '愛浪漫 C 行程   $ 4380   起/人'
       }
     ]
   }
